7b5de1ecaaf8d3c5b45fca45a4a4965dd18bc69f
stomaras <sptoma724@gmail.com>
2022-03-04 16:41:51 +0200

    JS data types and typeof keyword in js

---
+++ Section1-javaScriptFundamentals/starter/script.js
@@ -0,0 +1,14 @@
+let js = 'amazing';
+
+console.log(6 + 3);
+
+let firstName = "Jonas";
+console.log(firstName)
+firstName = 3
+console.log(firstName)
+
+// example of undefined
+let year;
+console.log(year);
+
+console.log(typeof null);
