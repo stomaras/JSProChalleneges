b7d035056730070dc8ee7e0ef7cb81d98f2522e8
stomaras <sptoma724@gmail.com>
2022-03-13 15:18:17 +0200

    Dom event handler

--- JSInTheBrowserDOMAndEventsFundamentals/ModalWindow/starter/script.js
+++ JSInTheBrowserDOMAndEventsFundamentals/ModalWindow/starter/script.js
@@ -1,24 +1,37 @@
 'use strict';
 
+const btnsOpen = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
 
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+for (var i = 0; i < btnsOpen.length; i++) {
+  btnsOpen[i].addEventListener('click', function () {
+    openModal();
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+modal.addEventListener('click', function () {
+  closeModal();
 });
 
 overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  closeModal();
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
 });
